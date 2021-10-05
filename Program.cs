850daea03e04aac23ab862696861db9308b9f797
Justin McCarthy <uvatmvf@gmail.com>
2021-10-05 16:40:47 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Add input data
+using LearnML;
+
+Console.WriteLine("Enter your review");
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = Console.ReadLine()
+};
+
+// Load model and predict output of sample data
+var result = SentimentModel.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+string sentiment = result.Prediction == 1 ? $"{result.Score[1]:P} Positive" : $"{result.Score[0]:P} Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
